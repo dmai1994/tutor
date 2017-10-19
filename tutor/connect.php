1fcc9e8752210a2b574aa74bbccb7a5e0727d77f
David Mai <31224704+dmai1994@users.noreply.github.com>
2017-10-19 18:20:45 -0400

    Update connect.php

--- tutor/connect.php
+++ tutor/connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $user = "dmai1994_user";
-$pass = "**********";
+$pass = "*******";
 $dbname = "dmai1994_tutor";
 
 $connection = mysqli_connect($servername, $user, $pass, $dbname);
@@ -13,6 +13,19 @@ if(!mysqli_connect($servername, $user,  $pass, $dbname))
 if(!mysqli_select_db($connection, $dbname))
 {
     exit('Error: could not select the database');
+}
+
+if(isset($_POST['ban'])) {
+  $banned = $_POST['ban'];
+  $sqlBan = "UPDATE Users SET status='banned' WHERE userID='$banned'";
+  mysqli_query($connection, $sqlBan);
+  header('Refresh: .1; URL = ban.php');
+}
+if(isset($_POST['unban'])) {
+  $unbanned = $_POST['unban'];
+  $sqlBan = "UPDATE Users SET status='user' WHERE userID='$unbanned'";
+  mysqli_query($connection, $sqlBan);
+  header('Refresh: .1; URL = ban.php');
 }
 
 ?>
